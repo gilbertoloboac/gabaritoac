2cb2a393841a14ad5ce8556a745af0d4fe5e0877
gilbertoloboac <loboacre@gmail.com>
2026-06-26 17:49:00 -0500

    fix: menu admin Notícias usa URL dinâmica ao invés de ID hardcoded
    O link /admin/pages/4/ estava hardcoded e não existia no servidor.
    Agora resolve o ID da NoticiaIndexPage dinamicamente.

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -5,9 +5,16 @@ from django.urls import reverse
 
 @hooks.register("register_admin_menu_item")
 def register_noticias_menu():
+    from noticias.models import NoticiaIndexPage
+
+    nip = NoticiaIndexPage.objects.first()
+    if nip:
+        url = reverse("wagtailadmin_explore", args=[nip.pk])
+    else:
+        url = reverse("wagtailadmin_explore_root")
     return MenuItem(
         "Notícias",
-        "/admin/pages/4/",
+        url,
         icon_name="list-ul",
         order=200,
     )
